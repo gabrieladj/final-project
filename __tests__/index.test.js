dc27a5e10842da6f4bf578b04bc3ce234d9f4ef6
gabrieladj <gabbiedjulabic@gmail.com>
2023-10-17 00:09:27 -0400

    added map test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import Home from '../src/app/page';
+import Map from '../src/app/page';
 import '@testing-library/jest-dom';
  
 describe('Home', () => {
   it('renders a heading', () => {
     render(<Home />)
- 
     expect(screen.getByText(`Humanitarianism Project`)).toBeInTheDocument()
+  });
+
+  it('renders the map', () => {
+    render(<Map />)
+    const mapImage = screen.getByAltText('Map'); // Use the 'name' attribute as the accessible name
+    expect(mapImage).toBeInTheDocument();
+    expect(mapImage).toHaveAttribute('src', '/Picture1.png');
   });
 });
--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,13 @@ export default function Home() {
   return (
     <div>
       <h2>Humanitarianism Project</h2>
-      <img src='Picture1.png' alt='image' />
+      <Map />
     </div>
   )
 }
+
+export function Map() {
+  return (
+    <img src="/Picture1.png" alt="Map"></img>
+  );
+};
