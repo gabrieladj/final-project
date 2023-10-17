a47b9e84f0077bfd93f8caa95e686aa4e5951f1b
gabrieladj <gabbiedjulabic@gmail.com>
2023-10-16 23:09:20 -0400

    added prisma schema

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,7 @@
-
-
-
 export default function Home() {
   return (
-    <h2>Humanitarianism Project</h2>
     <div>
+      <h2>Humanitarianism Project</h2>
       <img src='Picture1.png' alt='image' />
     </div>
   )
