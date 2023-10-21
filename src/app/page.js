24a2d30d4a30216cbe38dbc4ae0afed06c3388af
ajh1043 <ajh1043@usnh.edu>
2023-10-20 20:51:34 -0400

    added canvas scaling

--- src/app/page.js
+++ src/app/page.js
@@ -9,31 +9,31 @@ import Head from 'next/head'
 export default function Main(props) {
   const canvasRef = React.useRef(null);
 
+  const defaultSize = 720;
+
   function drawForeground(ctx) {
     const width = ctx.canvas.width;
     const height = ctx.canvas.height;
 
+    const camp1 = { x: 185, y: 45 };
+    const camp2 = { x: 150, y: 145 };
+    const campSize = 20;
+
     ctx.fillStyle = '#0000CC';
+    ctx.strokeStyle = '#e8bd20';
     ctx.beginPath();
-    ctx.fillRect(200,40, 20, 20)
-    //ctx.arc(100, 100, 20, 0, 2*Math.PI);
-    //ctx.fill();
+    ctx.moveTo(camp1.x,camp1.y);
+    ctx.lineTo(camp2.x,camp2.y);
+    ctx.lineWidth = 5;
+    ctx.stroke(); // Render the path
+    ctx.fillRect(camp1.x-campSize/2, camp1.y-campSize/2, campSize, campSize);
+    ctx.fillRect(camp2.x-campSize/2, camp2.y-campSize/2, campSize, campSize);
+    
+
   }
   
   function draw(ctx) {
-    const width = ctx.canvas.width;
-    const height = ctx.canvas.height;
-    console.log(width + ", " + height);
-    ctx.clearRect(0, 0, width, height);
-    // ctx.drawImage(background, 0, 0, background.width, background.height,
-    // 0, 0, ctx.width, ctx.height); // destination rectangle;
-    var background = new Image();
-    background.src = '/map.png'
-    background.onload = function() {
-        //ctx.drawImage(background,0,0);
-        drawImageProp(ctx, background, 0, 0, width, height);
-        drawForeground(ctx);
-    }
+    drawForeground(ctx);
   }
 
   useEffect(() => {
@@ -41,16 +41,21 @@ export default function Main(props) {
     const ctx = canvas.getContext('2d');
     window.addEventListener('resize', resizeCanvas, false);
     function resizeCanvas() {
+      const pixelRatio = 1;//window.devicePixelRatio || 1;
+      var size = defaultSize;
       // we want a square canvas
       // which dimension is smaller?
       if (window.innerWidth < window.innerHeight) {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerWidth;
+        size = window.innerWidth;
+        canvas.width = size;
+        canvas.height = size;
       }
       else {
-        canvas.width = window.innerHeight;
-        canvas.height = window.innerHeight;
+        size = window.innerHeight;
+        canvas.width = size;
+        canvas.height = size;
       }
+      ctx.scale(size/defaultSize, size/defaultSize);
       draw(ctx)
     }
     resizeCanvas();
@@ -59,13 +64,11 @@ export default function Main(props) {
 
 
   return (
-    <div>
-      <div className={styles.Canvas2D}>
-        <canvas data-testid="canvas"
-        ref={canvasRef}
-        width={600}
-        height={600}
-        />
+    <div className="centered-container">
+      <div id="canvas-container" className={styles.Canvas2D}>
+        {/* Add a background image as a CSS background */}
+        <div className="CanvasBackground" />
+        <canvas data-testid="canvas" ref={canvasRef} width={defaultSize} height={defaultSize} />
       </div>
     </div>
   );
